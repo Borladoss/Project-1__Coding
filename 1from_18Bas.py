390d1d3bb4dc4cdd8fd73217a2c2a1a4773e7abe
Borladoss <patadanai25510@gmail.com>
2024-12-04 16:07:42 +0700

    Add files via upload

---
+++ 1from_18Bas.py
@@ -0,0 +1,11 @@
+print("Hello sir")
+
+print("\n\กรอกข้อมูลตนเอง")
+name = input("Name : ")
+password = input("Password : ")
+select_game = input("Select Game : ")
+
+print("\n\Wellcome" , name)
+print("\n\Enter your password" , password)
+print("\n\Select game" , select_game)
+print("\n\Thank You")
